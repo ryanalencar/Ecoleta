e2fd29c6ad27c60a65ed602bc4b36dc849088427
Ryan Alencar <31630354+ryanalencar@users.noreply.github.com>
2020-06-02 14:24:28 -0300

    feat: frontend initital files and eslint/prettier config

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,9 +1,9 @@
 import express from 'express';
+import routes from './routes';
 
 const app = express();
 
-app.get('/users', (req, res) => {
-  res.send('hello NLW');
-})
+app.use(express.json());
+app.use(routes);
 
 app.listen(3333);
